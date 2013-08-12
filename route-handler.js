d10a27ab70da009335b05972549b7a6783e43377
RolandStuder <roland.studer@gmail.com>
2013-08-12 18:18:32 +0200

    getting closer for DOM update, but not quite

--- route-handler.js
+++ route-handler.js
@@ -1,8 +1,20 @@
 
 Meteor.Router.add({
-  '': 'index',
   '*': function() {
   	page = this.pathname.slice(1,this.pathname.length); // cut of leading '/'
-  	return Template[page] ? page : 'not_found'
+  	page = page || 'index';
+    page = Template[page] ? page : "not_found";
+    Session.set('currentPage', page);
   }
 });
+
+Handlebars.registerHelper('renderPage', function(name, options) {
+    return new Handlebars.SafeString(Template[Session.get('currentPage')]());
+});
+
+
+Deps.autorun(function() {
+  console.log(Session.get('currentPage'));
+  $("a").addClass("active");
+  $(".nav > li").addClass("active");
+})
