701f0d908db188f4048836873350b99b05146b7e
RolandStuder <roland.studer@gmail.com>
2013-08-14 20:31:56 +0200

    working flickr image replacer

--- route-handler.js
+++ route-handler.js
@@ -11,12 +11,14 @@ Meteor.Router.add({
 
 
 Meteor.startup(function() {
+
   Template.renderPage.content = function() {
     return new Handlebars.SafeString(Template[Session.get('currentPage')]());
   }
 
   Template.renderPage.rendered = function() {
-    addActiveClassToLinks()
+    addActiveClassToLinks();
+    replaceImagePlaceholders();
   }
 });
 
@@ -32,3 +34,41 @@ addActiveClassToLinks = function() {
     }
   });
 }
+
+var replaceImagePlaceholders = function() {
+  replacePlaceholders = function() {
+      var flickrAPIkey = '1a02addb94c985970bca4339af022b01';
+
+      $.ajax({
+        url: "http://ycpi.api.flickr.com/services/rest/",
+        data: { 
+          api_key: flickrAPIkey,
+          format: 'json',
+          method: 'flickr.photos.search',
+          license: 4, // CC attribution license
+          text: 'color',
+          safe_search: 3
+        },
+        dataType: 'jsonp',
+        jsonp: 'jsoncallback',
+        success: function(response) {
+          replaceNow(response.photos.photo);
+        }
+      });
+
+      replaceNow = function(photoArray) {
+        $("img[src^='flickr://']").each(function(index){
+          currentPhoto = photoArray[index];
+          photoURL = "http://farm" + currentPhoto.farm + ".staticflickr.com/" + currentPhoto.server + "/" + currentPhoto.id + "_" + currentPhoto.secret + ".jpg";
+          // http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
+          $(this).attr('src', photoURL);
+          Session.set('photos', photoArray);
+        });
+      }
+  }
+
+  if ($("img[src^='flickr://']").length) replacePlaceholders();
+
+
+}
+
